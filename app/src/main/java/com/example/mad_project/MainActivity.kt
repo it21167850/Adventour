4e6ca238bf8bb9a02c5f528490ca56943b18149b
IT21172182GLCNDWijayawardhana <it21172182@my.sliit.lk>
2023-05-01 22:37:33 +0530

    all one

--- app/src/main/java/com/example/mad_project/MainActivity.kt
+++ app/src/main/java/com/example/mad_project/MainActivity.kt
@@ -1,25 +1,11 @@
 package com.example.mad_project
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.english_guide)
-
-//        val signinbtn = findViewById<Button>(R.id.signin)
-//        val signupbtn = findViewById<Button>(R.id.signup)
-//        signupbtn.setOnClickListener {
-//            val intent = Intent(this, SignUp::class.java)
-//            startActivity(intent)
-//        }
-//        signinbtn.setOnClickListener{
-//            val intent = Intent(this,SignIn::class.java)
-//            startActivity(intent)
-//        }
+        setContentView(R.layout.admin_dashboard)
     }
 }
