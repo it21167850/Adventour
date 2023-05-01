8f7400221ac229d530b26f13c6141f6d05244cf9
Hansana K.T <it21167850@my.sliit.lk>
2023-05-01 11:41:42 +0530

    create app

--- app/src/main/java/com/example/mad_project/MainActivity.kt
+++ app/src/main/java/com/example/mad_project/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.travel_cart)
+        setContentView(R.layout.add_tourguide_to_cart)
     }
 }
