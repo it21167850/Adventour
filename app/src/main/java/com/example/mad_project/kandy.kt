1bc8047261d80bb0826635b3508ff3530cefc0fa
IT21183218 <99754850+IT21183218@users.noreply.github.com>
2023-05-04 22:35:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/mad_project/kandy.kt
@@ -0,0 +1,11 @@
+package com.example.mad_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kandy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kandy)
+    }
+}
